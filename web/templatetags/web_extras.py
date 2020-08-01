e6d9ad9e9e72418be360a626fef7e1a62d3b7939
m0tz <agustin_enriquez@hotmail.com>
2020-08-01 14:54:34 -0300

    templatetags test

--- web/templatetags/web_extras.py
+++ web/templatetags/web_extras.py
@@ -13,4 +13,9 @@ def process_course_image_path_script():
 
 @register.simple_tag()
 def print_console_log():
-    return "console.log(\'Hello world, im coding js using python\')"
+    return "console.log('Hello world, im coding js using python')"
+
+
+@register.simple_tag()
+def test_jquery():
+    return "$('div').toArray().forEach((e)=>{console.log(e)})"
